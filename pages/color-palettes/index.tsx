d7f43b603119112b44867361b44d528b369dd25a
zomdar <zomdar@gmail.com>
2021-08-11 18:10:18 -0500

    adding color palette stuff

--- pages/color-palettes/index.tsx
+++ pages/color-palettes/index.tsx
@@ -3,6 +3,52 @@ import * as React from "react";
 import NavBar from "../components/common/NavBar";
 import Head from "next/head";
 import { TrendingUpIcon, CodeIcon } from "@heroicons/react/solid";
+import ColorPalette, {
+  ColorPaletteLayout,
+} from "../components/common/ColorPaletteLayout";
+
+const someColorPalettes: ColorPaletteLayout[] = [
+  {
+    type: "cold palette",
+    level: "001",
+    colorCode: ["#283044", "#78A1BB", "#EBF5EE", "#BFA89E", "#8B786D"],
+  },
+  {
+    type: "cold palette",
+    level: "002",
+    colorCode: ["#82D4BB", "#82C09A", "#82AC9F", "#829298", "#94778B"],
+  },
+  {
+    type: "cold palette",
+    level: "003",
+    colorCode: ["#DABFFF", "#907AD6", "#4F518C", "#2C2A4A", "#7FDEFF"],
+  },
+  {
+    type: "cold palette",
+    level: "004",
+    colorCode: ["#274060", "#335C81", "#65AFFF", "#1B2845", "#5899E2"],
+  },
+  {
+    type: "warm palette",
+    level: "001",
+    colorCode: ["#4F000B", "#720026", "#CE4257", "#FF7F51", "#FF9B54"],
+  },
+  {
+    type: "warm palette",
+    level: "002",
+    colorCode: ["#DDE8B9", "#E8D2AE", "#D7B29D", "#CB8589", "#796465"],
+  },
+  {
+    type: "warm palette",
+    level: "003",
+    colorCode: ["#F08080", "#F4978E", "#F8AD9D", "#FBC4AB", "#FFDAB9"],
+  },
+  {
+    type: "warm palette",
+    level: "004",
+    colorCode: ["#8E9AAF", "#CBC0D3", "#EFD3D7", "#FEEAFA", "#DEE2FF"],
+  },
+];
 
 export default function Landing() {
   return (
@@ -23,7 +69,7 @@ export default function Landing() {
             <div className="flex flex-row">
               <div className="flex flex-row">
                 <TrendingUpIcon className="h-5 w-5 text-gray-500" />
-                <p className="pl-2 text-gray-200">10 Color Gradients</p>
+                <p className="pl-2 text-gray-200">8 Color Palettes</p>
               </div>
               <div className="flex flex-row pl-4">
                 <CodeIcon className="h-5 w-5 text-gray-500" />
@@ -34,7 +80,24 @@ export default function Landing() {
         </div>
       </div>
       <div className="container mx-auto px-4 pt-16">
-        <p className="text-xl">Coming Soon!</p>
+        <p className="text-xl pb-24">Cold Color Palettes</p>
+        {someColorPalettes
+          .filter((somePalette) => somePalette.type === "cold palette")
+          .map((filteredPalette: ColorPaletteLayout) => (
+            <ColorPalette
+              palette={filteredPalette}
+              key={filteredPalette.colorCode[0]}
+            ></ColorPalette>
+          ))}
+        <p className="text-xl py-24">Warm Color Palettes</p>
+        {someColorPalettes
+          .filter((somePalette) => somePalette.type === "warm palette")
+          .map((filteredPalette: ColorPaletteLayout) => (
+            <ColorPalette
+              palette={filteredPalette}
+              key={filteredPalette.colorCode[0]}
+            ></ColorPalette>
+          ))}
       </div>
     </>
   );
---
+++ pages/components/common/ColorPaletteLayout.tsx
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+
+export interface ColorPaletteLayout {
+  type: string;
+  level: string;
+  colorCode: string[];
+}
+
+const ColorPalette = (props: { palette: ColorPaletteLayout }) => {
+  const [copied, setCopied] = useState(false);
+  const [selectedColor, setSelectedColor] = useState('');
+
+  const { palette } = props;
+
+  if (!palette) {
+    return null;
+  }
+
+  return (
+    <div className="flex flex-row pb-16">
+      <div className="flex flex-col">
+        <p className="text-l">{palette.type.toLocaleUpperCase()}</p>
+        <p className="text-md pt-4">{palette.level}</p>
+      </div>
+      <div className="flex flex-wrap pl-14">
+        {palette.colorCode.map((color) => (
+          <div className="flex flex-col pr-4" key={color}>
+            <button
+              type="button"
+              className="py-8 px-24 rounded"
+              style={{ backgroundColor: color }}
+              onClick={() => {
+                setCopied(true);
+                setSelectedColor(color);
+                setTimeout(function () {
+                  setCopied(false);
+                }, 1000);
+                navigator.clipboard.writeText(color);
+              }}
+            ></button>
+            <p className="text-md text-gray-400 pt-2">{color}</p>
+            {(copied && color == selectedColor)  ? (
+              <p className="text-xs text-red-500 animate-pulse">Copied!</p>
+            ) : (
+              ""
+            )}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ColorPalette;
